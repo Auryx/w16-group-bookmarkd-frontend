c49210b8c12d766139dfb2c0f65fbb8a5590db99
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-09 20:48:05 -0400

    setup show page

---
+++ src/pages/Show.js
@@ -0,0 +1,28 @@
+import { useLoaderData } from "react-router-dom";
+
+function Show(props) {
+
+    const bookmark = useLoaderData();
+    // console.log(bookmark)
+
+    return (
+        <div className="bookmark">
+            <h1>{bookmark.name}</h1>
+            <h2>{bookmark.url}</h2>
+
+            <h2>Update {bookmark.name}</h2>
+            <Form action={`/update/${bookmark._id}`} method='post'>
+                <input type='text' name='name' placeholder="bookmark name" defaultValue={bookmark.name} />
+                <input type='text' name='url' placeholder="bookmark url" defaultValue={bookmark.url} />
+                <input type='submit' value="Update URL" />
+            </Form>
+
+            <h2>Delete Bookmark</h2>
+            <Form action={`/delete/${bookmark._id}`} method='post'>
+                <input type='submit' value='Delete Bookmark' />
+            </Form>
+        </div>
+    )
+}
+
+export default Show;
